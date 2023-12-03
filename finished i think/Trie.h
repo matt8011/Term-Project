b09f44522f3f076ec9f8682fd6fae19ddcfa201d
matt kyle <123500841+matt8011@users.noreply.github.com>
2023-12-03 18:00:59 -0500

    Add files via upload

---
+++ finished i think/Trie.h
@@ -0,0 +1,49 @@
+#include <ostream>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+
+#ifndef Trie_h
+#define Trie_h
+
+class Trie {
+public:
+    Trie();
+    std::stringstream dictionary;
+
+    int insert(std::string key);
+    void fileWrite(std::string ofname);
+
+
+    bool strSearch(std::string value);
+
+    std::vector<std::string> autoComp(std::string val);
+    void autoFileWrite(std::string ofname, std::vector< std::pair<std::string, std::vector<std::string>> > optionList);
+    void searchFileWrite(std::string ofname,std::vector< std::pair<std::string, bool> > searchList) ;
+
+private:
+    struct node {
+        char data;
+        std::vector<node*> subs;
+        bool word;
+
+        node(char data) : data(data), word(false), subs({}) {}
+    };
+    std::stringstream stream;
+    node* root;
+    std::vector<std::string> compOptions;
+    int autoSize;
+
+    void filePrint(node* rootNode);
+
+    void insert(std::string key, bool priv);
+
+    void compSearch(node *rootNode, int& index, std::string prefix);
+    void countLeaves(node *rootNode);
+
+
+
+};
+
+#endif //Trie_h
